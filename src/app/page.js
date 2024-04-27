5a76f33475c79e0aec20d6f70118b9c0c94f92f7
MAHARSHI025 <maharshipatel3851@gmail.com>
2024-04-27 22:39:02 +0530

    error

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script>
+      <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module" async></script>
 
       <div className="main flex justify-center gap-6 flex-wrap">
         <img src="/images/483013_5394652_949707_image.png" alt="" className=" h-60" />
@@ -17,7 +17,7 @@ export default function Home() {
 
       <div className="info flex flex-wrap-reverse items-center mx-4">
         <div className="area flex flex-col justify-center ">
-          <h1 className=" text-3xl font-bold">Explore World's mind</h1>
+          <h1 className=" text-3xl font-bold">Explore Worlds mind</h1>
           <h1 className=" ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, sapiente accusamus. Exercitationem, maiores. Laborum, explicabo nam? Labore ipsum inventore laborum!</h1>
         </div>
         <div className="lootie">
@@ -29,7 +29,7 @@ export default function Home() {
           <dotlottie-player src="https://lottie.host/8c3dc4fb-72f2-4252-90c0-bed6fc99b450/DNUtt9Xel9.json" background="transparent" speed="1" direction="1" playMode="normal" loop autoplay></dotlottie-player>
         </div>
         <div className="area flex  flex-col justify-center">
-          <h1 className=" text-3xl font-bold">Explore World's mind</h1>
+          <h1 className=" text-3xl font-bold">Explore Worlds mind</h1>
           <h1 className=" ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, sapiente accusamus. Exercitationem, maiores. Laborum, explicabo nam? Labore ipsum inventore laborum!</h1>
         </div>
       </div>
