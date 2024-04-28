e62a66e5f397082f9868896036d859b27484b081
MAHARSHI025 <maharshipatel3851@gmail.com>
2024-04-28 10:02:22 +0530

    ui changes

--- src/app/page.js
+++ src/app/page.js
@@ -10,27 +10,88 @@ export default function Home() {
           <h1 className=" text-center text-5xl font-bold">Thought is Forever</h1>
           <div className="btn">
             <Link href={"/thoughts"}><button type="submit" className=' my-2 border-2 px-4 py-1 rounded-lg mainbtn'>Explore</button></Link>
-            <Link href={"/"}><button type="submit" className=' my-2 border-2 px-4 py-1 rounded-lg subbtn'>Read about</button></Link>
+            <Link href={"#about"}><button type="submit" className=' my-2 border-2 px-4 py-1 rounded-lg subbtn'>Read about</button></Link>
           </div>
         </div>
       </div>
 
-      <div className="info flex flex-wrap-reverse items-center mx-4">
-        <div className="area flex flex-col justify-center ">
-          <h1 className=" text-3xl font-bold">Explore Worlds mind</h1>
-          <h1 className=" ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, sapiente accusamus. Exercitationem, maiores. Laborum, explicabo nam? Labore ipsum inventore laborum!</h1>
+      <div>
+        <div className="info bgier flex flex-wrap-reverse justify-center items-center mx-4">
+          <div className="area flex flex-col justify-center gap-4">
+            <h1 className=" text-3xl font-bold">Explore Worlds mind</h1>
+            <h1 className=" font-extralight text-xs" >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta, magnam. sapiente accusamus. Exercitationem, maiores. Laborum, explicabo nam? Labore ipsum inventore laborum!</h1>
+          </div>
+          <div className="lootie ">
+            <dotlottie-player src="https://lottie.host/8c0d9651-94d8-46ad-9f3e-d06ef100a470/ClMzMGK47k.json" background="transparent" speed="1" direction="1" playMode="normal" loop autoplay></dotlottie-player>
+          </div>
         </div>
-        <div className="lootie">
-          <dotlottie-player src="https://lottie.host/d3624a05-f580-4d44-bbe1-962960ed32c1/qRUzYbADPN.json" background="transparent" speed="1" direction="1" playMode="normal" loop autoplay></dotlottie-player>
+        <div className="info info2 flex  justify-center items-center mx-4">
+          <div className="lootie lootie2">
+            <dotlottie-player src="https://lottie.host/e7c954e1-040b-43a0-9616-bf80c6f56dfb/xh0yxjcu9o.json" background="transparent" speed="1" direction="1" playMode="normal" loop autoplay></dotlottie-player>
+          </div>
+          <div className="area flex  flex-col justify-center gap-4">
+            <h1 className=" text-3xl font-bold">Explore Worlds mind</h1>
+            <h1 className=" font-extralight text-xs" >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta, magnam. sapiente accusamus. Exercitationem, maiores. Laborum, explicabo nam? Labore ipsum inventore laborum!</h1>
+          </div>
         </div>
       </div>
-      <div className="info info2 flex  items-center mx-4">
-        <div className="lootie">
-          <dotlottie-player src="https://lottie.host/8c3dc4fb-72f2-4252-90c0-bed6fc99b450/DNUtt9Xel9.json" background="transparent" speed="1" direction="1" playMode="normal" loop autoplay></dotlottie-player>
-        </div>
-        <div className="area flex  flex-col justify-center">
-          <h1 className=" text-3xl font-bold">Explore Worlds mind</h1>
-          <h1 className=" ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, sapiente accusamus. Exercitationem, maiores. Laborum, explicabo nam? Labore ipsum inventore laborum!</h1>
+
+      <div className="about" id="about">
+        <h1 className=" text-center text-5xl font-bold">How It Works</h1>
+        <div className="section flex gap-2 justify-center flex-col">
+          <div className="column">
+            <div data-aos="fade-in" className=" flex flex-row justify-center items-center">
+              <div className="first w-48 flex justify-end items-center"><h1 className='number text-center flex justify-center items-center '>1</h1></div>
+              <hr />
+              <div className="line"></div>
+              <hr />
+              <div className="text w-48 flex  flex-col justify-center items-center gap-2">
+                <h1 className=" text-xl font-bold">Signup or login</h1>
+                <h1 className=' text-center font-medium text-xs'>First I design my project in figma and made user-friendly websites.</h1>
+              </div>
+            </div>
+          </div>
+          <div data-aos="fade-in" className=" flex flex-row justify-center items-center">
+            <div className="text w-48 flex  flex-col justify-center items-center">
+              <h1 className=" text-xl font-bold">Put your Thought</h1>
+              <h1 className=' text-center font-medium text-xs'>Then I code basic structure of my website using html elements and css</h1>
+            </div>
+            <hr />
+            <div className="line"></div>
+            <hr />
+            <div className="first w-48 flex justify-start items-center"><h1 className='number text-center flex justify-center items-center'>2</h1></div>
+          </div>
+          <div data-aos="fade-in" className=" flex flex-row justify-center items-center">
+            <div className="first w-48 flex justify-end items-center"><h1 className='number text-center flex justify-center items-center'>3</h1></div>
+            <hr />
+            <div className="line"></div>
+            <hr />
+            <div className="text w-48 flex  flex-col justify-center items-center">
+              <h1 className=" text-xl font-bold">See World thinking</h1>
+              <h1 className=' text-center font-medium text-xs'>I make logic in javascript to perfome a better website experience for user</h1>
+            </div>
+          </div>
+          <div data-aos="fade-in" className=" flex flex-row justify-center items-center">
+            <div className="text w-48 flex  flex-col justify-center items-center">
+              <h1 className=" text-xl font-bold">Update thought</h1>
+              <h1 className=' text-center font-medium text-xs'>I choose react.js library to make web-app for my project. It was a sufficent library to code in js.</h1>
+            </div>
+            <hr />
+            <div className="line"></div>
+            <hr />
+            <div className="first w-48 flex justify-start items-center"><h1 className='number text-center flex justify-center items-center'>4</h1></div>
+          </div>
+          <div data-aos="fade-in" className=" flex flex-row justify-center items-center">
+            <div className="first w-48 flex justify-end items-center"><h1 className='number text-center flex justify-center items-center'>5</h1></div>
+            <hr />
+            <div className="line"></div>
+            <hr />
+            <div className="text w-48 flex  flex-col justify-center items-center">
+              <h1 className=" text-xl font-bold">Be exploring</h1>
+              <h1 className=' text-center font-medium text-xs'>Then I add tailwind css in my project for better looking website.</h1>
+            </div>
+          </div>
+          
         </div>
       </div>
     </>
